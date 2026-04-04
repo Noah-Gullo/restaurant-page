567df89626f449ab035147d5abee3f1ebf36ccec
Noah Gullo <noahgulloingram@gmail.com>
2026-04-03 20:52:45 -0700

    Add image file and attribution

---
+++ README.md
@@ -0,0 +1,4 @@
+# Restaurant Page
+
+## Credit
+[Background image](https://free-vectors.net/backgrounds/tropical-forest-vector) sourced from [FREE-VECTORS.NET](https://free-vectors.net/).
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,5 +1,9 @@
+import backgroundImage from "./background.png";
 export let homeContent = document.createElement("div");
+
 homeContent.setAttribute("id", "homeContent");
 const title = document.createElement("h1");
 title.textContent = "Elvin Emporium";
-homeContent.appendChild(title); 
+document.body.style.backgroundImage = backgroundImage;
+
+homeContent.appendChild(title);
