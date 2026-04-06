85c238073164bd4e3a9517e5c5dabc340d499a70
Noah Gullo <noahgulloingram@gmail.com>
2026-04-05 21:51:17 -0700

    Add placeholder item and price images

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,5 @@
+import placeholderItem from "../images/placeholderitem.png"
+import placeholderCoin from "../images/placeholdercoin.png";
 export let menuContent = document.createElement("div");
 menuContent.setAttribute("id", "menuContent");
 const title = document.createElement("h1");
@@ -7,10 +9,10 @@ menuGrid.setAttribute("id", "menuGrid");
 const dishNames = ["Firewyrm Ember Stew", "Kraken Ink Noodles", "Griffon Wing Skewers", "Phoenix Ash Bread", 
                    "Basilisk Egg Tart", "Serpentfire Curry",]
 const dishDescriptions = ["Slow-braised wrym meat with ashroot and ember pepper.", 
-                          "Slick strands tossed in deep-seaa spices and shadow oil.",
+                          "Slick strands tossed in deep-sea spices and shadow oil.",
                           "Charred with saffron spice and dusted in sky-salt.",
-                          "Tangy, golden, and faintly crackling with energy.",
-                          "Warm loaves that glow faintly and never truly burn.",
+                          "Tangy, golden, and faintly crackling with glowing energy.",
+                          "Warm loaves that glow faintly and never truly burns.",
                           "Blazing hot stew brewed from jungle herbs and scaled meats.",];     
 
 const populateGrid = function(){
@@ -18,12 +20,15 @@ const populateGrid = function(){
         const item = document.createElement("h3");
         item.setAttribute("id", "item"+i);
         item.textContent = dishNames[i];
-        const itemImg= document.createElement("img");
+        const itemImg = document.createElement("img");
+        itemImg.src = placeholderItem;
         const itemDescription = document.createElement("h4");
         itemDescription.textContent = dishDescriptions[i];
-
+        const currencyImg = document.createElement("img");
+        currencyImg.src = placeholderCoin;
         item.appendChild(itemImg);
         item.appendChild(itemDescription);
+        item.append(currencyImg);
         menuGrid.appendChild(item);
     }                     
 }
