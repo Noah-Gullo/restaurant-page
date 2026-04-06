092ab6a013364d6c933b6900745ff4724f169285
Noah Gullo <noahgulloingram@gmail.com>
2026-04-05 20:40:19 -0700

    Add homepage content

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,7 +1,18 @@
+import star from "../images/star.png";
 export let homeContent = document.createElement("div");
 
 homeContent.setAttribute("id", "homeContent");
 const title = document.createElement("h1");
 title.textContent = "Elvin Emporium";
+const starImage = document.createElement("img");
+starImage.src = star;
+const headline = document.createElement("h2");
+headline.textContent = "Welcome to the finest tavern!"
+const description = document.createElement("p");
+description.textContent = "Treat your taste to the widest array of pleasures within any land with rare scents upon this air. \
+Within this fine establishment lay sizzling meat from beasts far off lands and curious delicacies from places yet unknown.";
 
 homeContent.appendChild(title);
+homeContent.appendChild(starImage);
+homeContent.appendChild(headline);
+homeContent.appendChild(description);
