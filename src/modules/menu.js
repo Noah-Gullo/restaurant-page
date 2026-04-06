479fb45b79953d35d50f173585fa2929ffdfc0e6
Noah Gullo <noahgulloingram@gmail.com>
2026-04-05 22:02:34 -0700

    Add random price to each menu item

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -6,29 +6,38 @@ const title = document.createElement("h1");
 title.textContent = "Menu";
 const menuGrid = document.createElement("div");
 menuGrid.setAttribute("id", "menuGrid");
-const dishNames = ["Firewyrm Ember Stew", "Kraken Ink Noodles", "Griffon Wing Skewers", "Phoenix Ash Bread", 
+const itemNames = ["Firewyrm Ember Stew", "Kraken Ink Noodles", "Griffon Wing Skewers", "Phoenix Ash Bread", 
                    "Basilisk Egg Tart", "Serpentfire Curry",]
-const dishDescriptions = ["Slow-braised wrym meat with ashroot and ember pepper.", 
+const itemDescriptions = ["Slow-braised wrym meat with ashroot and ember pepper.", 
                           "Slick strands tossed in deep-sea spices and shadow oil.",
                           "Charred with saffron spice and dusted in sky-salt.",
                           "Tangy, golden, and faintly crackling with glowing energy.",
                           "Warm loaves that glow faintly and never truly burns.",
-                          "Blazing hot stew brewed from jungle herbs and scaled meats.",];     
+                          "Blazing hot stew brewed from jungle herbs and scaled meats.",]; 
+
+const itemPrices = ["100", "500", "250", "75", "125", "50", "1000"];
 
 const populateGrid = function(){
-    for(let i = 0; i < dishNames.length; i++){
+    for(let i = 0; i < itemNames.length; i++){
         const item = document.createElement("h3");
         item.setAttribute("id", "item"+i);
-        item.textContent = dishNames[i];
+        item.textContent = itemNames[i];
         const itemImg = document.createElement("img");
         itemImg.src = placeholderItem;
         const itemDescription = document.createElement("h4");
-        itemDescription.textContent = dishDescriptions[i];
+        itemDescription.textContent = itemDescriptions[i];
+        const priceContainer = document.createElement("div");
+        priceContainer.setAttribute("class", "price");
         const currencyImg = document.createElement("img");
         currencyImg.src = placeholderCoin;
+        const itemPrice = document.createElement("p");
+        itemPrice.textContent = "x" + itemPrices[i];
+        priceContainer.appendChild(currencyImg);
+        priceContainer.appendChild(itemPrice);
+
         item.appendChild(itemImg);
         item.appendChild(itemDescription);
-        item.append(currencyImg);
+        item.append(priceContainer);
         menuGrid.appendChild(item);
     }                     
 }
